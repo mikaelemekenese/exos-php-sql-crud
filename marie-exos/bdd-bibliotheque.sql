56bce89659136b56e952fc52b7d952585c581d96
Mikaele Mekenese <m.mekenese@cci-formation.nc>
2021-07-03 03:17:27 +1100

    ajout bdd

--- marie-exos/bdd-bibliotheque.sql
+++ marie-exos/bdd-bibliotheque.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 02 juil. 2021 à 13:21
+-- Généré le : ven. 02 juil. 2021 à 18:15
 -- Version du serveur :  10.4.19-MariaDB
 -- Version de PHP : 7.4.19
 
@@ -69,7 +69,7 @@ CREATE TABLE `emprunt` (
 INSERT INTO `emprunt` (`id`, `id_livre`, `id_adherent`, `date_emprunt`, `date_retourmax`, `date_retour`) VALUES
 (10, 4, 1, '2021-07-02', '2021-07-16', '2021-07-06'),
 (11, 2, 1, '2021-06-28', '2021-07-12', '2021-07-05'),
-(12, 1, 1, '2021-06-25', '2021-07-09', '2021-07-01');
+(12, 1, 1, '2021-06-25', '2021-07-09', '2021-07-09');
 
 -- --------------------------------------------------------
 
@@ -90,7 +90,7 @@ CREATE TABLE `livre` (
 --
 
 INSERT INTO `livre` (`id`, `titre`, `auteur`, `disponible`, `id_rayon`) VALUES
-(1, 'Harry Potter', 'J.K. Rowling', 1, 1),
+(1, 'Harry Potter', 'J.K. Rowling', 0, 1),
 (2, 'Ça', 'Stephen King', 0, 2),
 (3, 'L\'Attaque des Titans', 'Hajime Isayama', 1, 3),
 (4, 'L\'Astronomie Pour les Nuls', 'Stephen P. Maran', 0, 4),
@@ -119,6 +119,26 @@ INSERT INTO `rayon` (`id`, `nom`, `reference`) VALUES
 (3, 'Manga', 'MAN'),
 (4, 'Sciences', 'SCI'),
 (5, 'Histoire', 'HIS');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `nom_utilisateur` varchar(30) NOT NULL,
+  `mdp` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `nom_utilisateur`, `mdp`) VALUES
+(1, 'Mika', 'm123'),
+(2, 'Biblio', 'azerty');
 
 --
 -- Index pour les tables déchargées
@@ -152,6 +172,12 @@ ALTER TABLE `rayon`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -178,6 +204,12 @@ ALTER TABLE `livre`
 --
 ALTER TABLE `rayon`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
